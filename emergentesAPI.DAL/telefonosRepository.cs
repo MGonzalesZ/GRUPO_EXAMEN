f1ed0db9d95fbf32e996c5b7e30f1b682ff1aeb9
LuciaBeVi <alymao96@gmail.com>
2023-09-25 16:02:31 -0400

    controller

--- emergentesAPI.DAL/telefonosRepository.cs
+++ emergentesAPI.DAL/telefonosRepository.cs
@@ -8,5 +8,6 @@ namespace emergentesAPI.DAL
 {
     public class telefonosRepository
     {
+
     }
 }
---
+++ emergentesAPI.WEBApi/Controllers/TelefonoController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using emergentesAPI.LN.Service;
+
+namespace emergentesAPI.WEBApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class TelefonoController : ControllerBase
+    {
+
+    }
