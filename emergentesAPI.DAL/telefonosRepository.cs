c0921f60411d7717df28b67d2b6bfc4b6f8d277b
MGonzalesZ <mauriciogzb1322@gmail.com>
2023-09-25 16:21:53 -0400

    REPOSITORIO DAL

--- emergentesAPI.DAL/telefonosRepository.cs
+++ emergentesAPI.DAL/telefonosRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using emergentesAPI.DAL.DataContext;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,51 @@ namespace emergentesAPI.DAL
 {
     public class telefonosRepository
     {
+        private readonly DbA9f478TecemergContext _dbContext;
+        public telefonosRepository(DbA9f478TecemergContext context)
+        {
+            _dbContext = context;
+        }
 
+        public void Eliminar(int id)
+        {
+            var telefono = _dbContext.Telefonos.FirstOrDefault(p => p.Id==id);
+            if (telefono != null)
+            {
+                _dbContext.Telefonos.Remove(telefono);
+                _dbContext.SaveChanges();
+            }
+        }
+
+        public Telefono Agregar(string nombre, string numero)
+        {
+            var modelo = new Telefono()
+            {
+                Nombre = nombre,
+                NumeroTelefonico = numero
+            };
+            //modelo.Contrasenia = BCrypt.Net.BCrypt.HashPassword(modelo.Contrasenia);
+            _dbContext.Telefonos.Add(modelo);
+            _dbContext.SaveChanges();
+            return modelo;
+        }
+        //public void Actualizar(Usuario modelo)
+        //{
+        //    modelo.Contrasenia = BCrypt.Net.BCrypt.HashPassword(modelo.Contrasenia);
+        //    _dbContext.Entry(modelo).State = EntityState.Modified;
+        //    _dbContext.SaveChanges();
+        //}
+
+
+        //public List<Usuario> ObtenerTodos()
+        //{
+        //    //IQueryable<Usuario> queryUsuarioSQL = _dbContext.Usuarios;
+
+        //    return _dbContext.Usuarios.ToList();
+        //}
+        //public Usuario ObtenerPorId(int id)
+        //{
+        //    return _dbContext.Usuarios.FirstOrDefault(p => p.IdUsuario == id);
+        //}
     }
 }
--- emergentesAPI.LN/telefonosService.cs
+++ emergentesAPI.LN/telefonosService.cs
@@ -8,5 +8,6 @@ namespace emergentesAPI.LN
 {
     public class telefonosService
     {
+
     }
 }
