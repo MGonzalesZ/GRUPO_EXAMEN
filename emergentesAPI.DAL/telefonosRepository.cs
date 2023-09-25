9754ce530390884d780ca8215dfc7bbd91aa4044
mauri0326 <mauriciogarciam9@gmail.com>
2023-09-25 16:20:20 -0400

    dal agregar

--- emergentesAPI.DAL/telefonosRepository.cs
+++ emergentesAPI.DAL/telefonosRepository.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using emergentesAPI.DAL;
+using emergentesAPI.DAL.DataContext;
+using emergentesAPI.ENTIDADES;
 
 namespace emergentesAPI.DAL
 {
     public class telefonosRepository
     {
+        private readonly DbA9f478TecemergContext _dbContext;
+        
+        public telefonosRepository(DbA9f478TecemergContext context)
+        {
+            _dbContext = context;
+        }
+
+        public Telefono Agregar(string nombre, string numeroTelefonico)
+        {
+            var modelo = new Telefono()
+            {
+                
+                Nombre = nombre,
+                NumeroTelefonico = numeroTelefonico
+            };
+            _dbContext.Telefonos.Add(modelo);
+            _dbContext.SaveChanges();
+            return modelo;
+        }
+
+        
 
     }
 }
