42d59196bf9d4f27ca9d020068ed5f3b9ebd55d2
LuciaBeVi <alymao96@gmail.com>
2023-09-25 16:07:25 -0400

    controllerTElf

--- emergentesAPI.WEBApi/Controllers/TelefonoController.cs
+++ emergentesAPI.WEBApi/Controllers/TelefonoController.cs
@@ -8,5 +8,39 @@ namespace emergentesAPI.WEBApi.Controllers
 
     public class TelefonoController : ControllerBase
     {
+        private readonly telefonosService _telefonosService;
 
+        public TelefonoController(telefonosService telefonoService)
+        {
+            _telefonoService = telefonoService;
+        }
     }
+
+    [HttpPost]
+    [Route("agregar")]
+    public IActionResult Agregar(string Nombre, string NumeroTelefonico)
+    {
+        var nuevoTelefono = _telefonoService.CrearUsuario(Nombre, NumeroTelefonico);
+
+        return CreatedAtAction(nameof(ObtenerPorId), new { id = nuevoTelefono.IdTelefono }, nuevoTelefono);
+    }
+
+
+    [HttpDelete]
+    [Route("eliminar")]
+    public IActionResult Eliminar(int id)
+    {
+        var telefono = _telefonoService.ObtenerTelefonoPorId(id);
+
+        if (telefono == null)
+        {
+            return NotFound();
+        }
+
+        _telefonoService.Eliminar(id);
+
+        return NoContent();
+    }
+
+
+}
